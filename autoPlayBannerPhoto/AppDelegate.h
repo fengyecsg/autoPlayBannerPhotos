656aa3d7a7d38ea539d02f61ea57c149446cf0c9
fengye <850379974@qq.com>
2014-06-21 20:27:06 +0800

    Initial Commit

---
+++ autoPlayBannerPhoto/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  autoPlayBannerPhoto
+//
+//  Created by csg on 14-6-21.
+//  Copyright (c) 2014年 蓝鸥科技. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ autoPlayBannerPhotoTests/autoPlayBannerPhotoTests.m
@@ -0,0 +1,34 @@
+//
+//  autoPlayBannerPhotoTests.m
+//  autoPlayBannerPhotoTests
+//
+//  Created by csg on 14-6-21.
+//  Copyright (c) 2014年 蓝鸥科技. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface autoPlayBannerPhotoTests : XCTestCase
+
+@end
+
+@implementation autoPlayBannerPhotoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
